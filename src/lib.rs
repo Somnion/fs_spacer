a45ace6ece66b68c1171c0f4f25fdac426b7b602
Michael Keßler <somnion@gmx.de>
2021-06-19 23:24:23 +0200

    Formatted output

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,19 @@ use std::ffi::CString;
 use std::ffi::CStr;
 use std::error::Error;
 
-pub fn canonicalize_path(file_path: &String) -> std::io::Result<PathBuf>{
-    return fs::canonicalize(file_path);
+use std::path::*;
+use std::mem;
+use std::os::unix::ffi::OsStrExt;
+
+
+#[non_exhaustive]
+pub struct SpaceUnit;
+
+impl SpaceUnit {
+    pub const KB: u64 = 1024;
+    pub const MB: u64 = 1024 * 1024;
+    pub const GB: u64 = 1024 * 1024 * 1024;
+    pub const TB: u64 = 1024 * 1024 * 1024 * 1024;
 }
 
 pub fn get_current_username() -> Result<String, String>{
@@ -17,20 +28,49 @@ pub fn get_current_username() -> Result<String, String>{
     let uid : libc::uid_t = unsafe { libc::getuid() };
     // Can fail
     let username = unsafe{ libc::getpwuid(uid) };
+    // C-pointer, can therefor be null
     if username.is_null(){
         return Err("Can't read current user name.".to_string());
     }
-
-    let username =
+    // to_owned() creates a copy of the username, separating it from the C-pointer
+    let rustified_username =
         unsafe  { CStr::from_ptr((*username).pw_name).to_str().to_owned() };
 
-    match username{
-        Ok(uname ) => return Ok(uname.to_string()),
-        Err(err) => return Err(err.to_string()),
+    return match rustified_username {
+        Ok(uname) => Ok(uname.to_string()),
+        Err(err) => Err(err.to_string()),
     }
 }
 
-#[cfg(test)]
+pub fn get_statvfs_for_path(dir_path: &str) -> Result<libc::statvfs, String> {
+    let dir = Path::new(dir_path);
+    if !dir.exists() {
+        return Err(format!("{} doesn't exist as a directory.", dir_path));
+    }
+
+    let dir_as_c_representation = CString::new(dir.as_os_str().as_bytes()).unwrap();
+    unsafe {
+        let mut stat: libc::statvfs = mem::zeroed();
+        return match libc::statvfs(dir_as_c_representation.as_ptr() as *const _,
+                                     &mut stat){
+            0 => Ok(stat),
+            _ => Err(format!("Could get stats for dir {}", dir_path)),
+        }
+    }
+}
+
+pub fn get_formatted_space(space: u64) -> Result<String, String> {
+    match space {
+        size if size < SpaceUnit::KB => Ok(format!("{} B", size)),
+        size if size < SpaceUnit::MB => Ok(format!("{} KB", size as u64 / SpaceUnit::KB as u64)),
+        size if size < SpaceUnit::GB => Ok(format!("{} MB", size as u64 / SpaceUnit::MB as u64)),
+        size if size < SpaceUnit::TB => Ok(format!("{} GB", size as u64 / SpaceUnit::GB as u64)),
+        size if size >= SpaceUnit::TB => Ok(format!("{} TB", size as u64 / SpaceUnit::TB as u64)),
+        _ => Err(format!("Can't process size {} for formatted output", space)),
+    }
+}
+
+/*#[cfg(test)]
 mod test {
     use crate::canonicalize_path;
 
@@ -39,4 +79,4 @@ mod test {
         let s = String::from("/Users/michaelkessler/Projects/streams");
         println!("{:?}", canonicalize_path(&s));
     }
-}
+}*/
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use fs_spacer::{canonicalize_path, get_current_username};
+use fs_spacer::{get_current_username, get_statvfs_for_path, SpaceUnit};
 use std::os::raw::c_char;
 
 extern crate libc;
@@ -9,17 +9,24 @@ use std::mem;
 use std::os::unix::ffi::OsStrExt;
 
 fn main(){
-    let s = String::from("/Users/michaelkessler/Projects/streams");
-    println!("{:?}", canonicalize_path(&s));
+    /*let s = String::from("/Users/michaelkessler/Projects/streams");
+    println!("{:?}", canonicalize_path(&s));*/
 
-    let dir = Path::new("/Users/michaelkessler");
+    /*let dir = Path::new("/Users/michaelkessler");
     let c_dir = CString::new(dir.as_os_str().as_bytes()).unwrap();
     unsafe{
         let mut stat: libc::statvfs = mem::zeroed();
         let ret: i32 = libc::statvfs(c_dir.as_ptr() as *const _, &mut stat);
         println!("{}", stat.f_bsize);
-    }
+    }*/
 
-    let x= get_current_username();
-    println!("{}", x.unwrap());
+    let stats = get_statvfs_for_path("/Users/michaelkessler");
+    let stats = stats.unwrap();
+    println!("{}", stats.f_bsize);
+
+
+    let current_username= get_current_username();
+    println!("{}", current_username.unwrap());
+
+
 }
